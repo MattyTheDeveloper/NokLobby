7124a4d8ae90b8b3337f770bb7efb8af3296b6b1
Hitthetarget55 <46300928+Hitthetarget55@users.noreply.github.com>
2020-04-12 17:21:10 +0100

    Update serverClickEvent.java
    Should work now, sorry for the delay.

--- NokLobby/src/main/java/org/Nokwok/Events/serverClickEvent.java
+++ NokLobby/src/main/java/org/Nokwok/Events/serverClickEvent.java
@@ -7,6 +7,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.material;
 
 import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
@@ -19,21 +20,25 @@ Main plugin;
     }
     public void onClick(InventoryClickEvent e){
             Player p = (Player) e.getWhoClicked();
-            // I know using performCommand(); is a bad way of doing it, I don't have a lot of time today so ill figure out a better way tomorrow.
-        if (e.getClickedInventory().getTitle().equals(ChatColor.translateAlternateColorCodes('&', "&7Server list"))){
-          switch (e.getCurrentItem().getType()) {
-              case FIREWORK:
-                  p.sendMessage(ChatColor.GREEN + "Sending you to Events.");
-                  p.performCommand("server smp");
-                  break;
-              case GOLD_SPADE:
-                  p.sendMessage((ChatColor.GREEN + "Sending you to SMP."));
-                  p.performCommand("server smp");
-                  break;
-              case DIRT:
-                  p.sendMessage(ChatColor.RED + "PLACEHOLDER - NOT CODED YET.");
-                  break;
-            }
+            String smpcmd = "server smp";
+            String eventscmd = "server events"
+            //  Should work now. -Hit
+        if (e.getView().getTitle().equals(ChatColor.translateAlternateColorCodes('&', "&7Server list"))){
+                if(e.getCurrentItem() == null){
+                    return;
+                }else if(e.getCurrentItem().getType().equals(Material.GOLD_SPADE)){
+                    p.performCommand(smpcmd);
+                    p.sendMessage(ChatColor.RED + "Sending you to SMP.")
+                }else if(e.getCurrentItem().getType().equals(Material.FIREWORK)){
+                    p.performCommand(eventscmd);
+                    p.sendMessage(ChatColor.RED + "You cannot connect to this server right now. An event is not active.")
+                }else if(e.getCurrentItem().getType().equals(Material.GRASS)){
+                    p.sendMessage(ChatColor.RED + "I cannot do this right now. If you are a developer+, please inspect the code under serverClickEvent.java!")
+                }
+               
+        
+        }
+           
           }
     }
 
