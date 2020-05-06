516dc5b1d81804ae135f86075a092dd51da860a4
Hitthetarget55 <46300928+Hitthetarget55@users.noreply.github.com>
2020-05-06 17:23:48 +0100

    Update Main.java

--- NokLobby/src/main/java/org/Nokwok/Main.java
+++ NokLobby/src/main/java/org/Nokwok/Main.java
@@ -63,7 +63,7 @@ public class Main extends JavaPlugin implements Listener {
         getServer().getPluginManager().registerEvents(new MobEvent(), this);
         getServer().getPluginManager().registerEvents(new InventoryEvent(this, chat), this);
         getServer().getPluginManager().registerEvents(new MiniHitEvent(), this);
-
+        getServer().getPluginManager().registerEvents(new DrownEvent(), this);
         getServer().getMessenger().registerOutgoingPluginChannel(this, "BungeeCord");
     }
 
